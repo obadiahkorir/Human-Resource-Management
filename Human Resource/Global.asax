f957f72ff8924ca1c005434176dca3e1f65b773f
Obadiah <obadiahkorir@dynasoft.co.ke>
2020-02-19 09:15:14 +0300

    My last commit

---
+++ Human Resource/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Human_Resource.MvcApplication" Language="C#" %>
---
+++ Human Resource/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Human_Resource.Startup))]
+namespace Human_Resource
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
